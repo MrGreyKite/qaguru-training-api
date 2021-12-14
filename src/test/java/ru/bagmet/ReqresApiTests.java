7f689fb03595e01670e918ede5d9ac9786e43b2f
Рудольф Багмет <nolde3@yandex.ru>
2021-12-14 05:27:07 +0400

    +API test rest-assured

--- src/test/java/ru/bagmet/ReqresApiTests.java
+++ src/test/java/ru/bagmet/ReqresApiTests.java
@@ -1,14 +1,15 @@
 package ru.bagmet;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class ReqresApiTests {
 
@@ -34,8 +35,58 @@ public class ReqresApiTests {
                 .get("/api/users/2")
                 .then()
                 .extract().path("data.id");
-        Assertions.assertThat(id).isEqualTo(2);
+        assertThat(id).isEqualTo(2);
 
+    }
+
+    @Test
+    void GetUnexcitedUser() {
+        given()
+                .contentType(JSON)
+                .when()
+                .get("/api/users/23")
+                .then()
+                .statusCode(404)
+                .body(is("{}"));
+    }
+
+    @Test
+    void successfulRegistration() {
+        JsonPath reg = given()
+                .contentType(JSON)
+                .body("{\"email\":  \"eve.holt@reqres.in\"," +
+                        "\"password\": \"pistol\"}")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(200)
+                .extract().jsonPath();
+        assertThat("id").isNotEmpty();
+        assertThat("token").isNotEmpty();
+    }
+
+    @Test
+    void UnSuccessfulRegistration() {
+        given()
+                .contentType(JSON)
+                .body("{\"email\":  \"sydney@fife\"}")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(400)
+                .contentType(JSON)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .contentType(JSON)
+                .when()
+                .delete("/api/users/2")
+                .then()
+                .statusCode(204)
+                .body(is(""));
     }
 
 }
